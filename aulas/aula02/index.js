c08d4ddc0fd33f7c3466420e024d5f6e84ee37c2
vitorisalves <vitorisalves1@gmail.com>
2024-08-20 21:57:23 -0300

    fazendo coisa pra porra

--- aulas/aula02/index.js
+++ aulas/aula02/index.js
@@ -3,7 +3,18 @@ const root = document.getElementById("root");
 /* componentes da aplicacao */
 
 function Cabecalho() {
+    const logo = Imagem("", "Logo da Aplicação");
+    const titulo = Titulo("Aluno Online");
+    const grupo1 = document.createElement("div");
+    grupo1.append(logo, titulo);
+
+    const pesquisar = InputSearch();
+    const icone = Imagem("", "Ícone de Pesquisar");
+    const grupo2 = document.createElement("div");
+    grupo2.append(pesquisar, icone);
+
     const header = document.createElement("header");
+    header.append(grupo1, grupo2);
     return header;
 }
 
@@ -12,8 +23,11 @@ function Principal() {
     return main;
 }
 
-function Rodape() {
+function Rodape(texto) {
+    const paragrafo = document.createElement("p");
+    paragrafo.innerText = texto
     const footer = document.createElement("footer");
+    footer.append(paragrafo);
     return footer;
 }
 
@@ -24,34 +38,144 @@ function Botao(nome) {
     return button;
 }
 
+function Input(id, tipo, rotulo, obrigatorio = false) {
+    const label = document.createElement("label");
+    label.setAttribute("for", id);
+    label.innerText = rotulo;
+    const input = document.createElement("input");
+    input.setAttribute("type", tipo);
+    input.setAttribute("id", id);
+    input.setAttribute("name", id);
+    if (obrigatorio) input.setAttribute("required", "");
+    const grupo = document.createElement("div");
+    grupo.append(label, input);
+    return grupo;
+}
+
+function InputSearch() {
+    const input = document.createElement("input");
+    input.setAttribute("type", "search");
+    input.setAttribute("placeholder", "Pesquisar...");
+    return input;
+}
+
+function Link(texto, rota) {
+    const link = document.createElement("a");
+    link.setAttribute("href", rota);
+    link.innerText = texto;
+    return link;
+}
+
+function Titulo(texto) {
+    const titulo = document.createElement("h1");
+    titulo.innerText = texto;
+    return titulo;
+}
+
+function Imagem(origem, texto) {
+    const img = document.createElement("img");
+    img.setAttribute("src", origem);
+    img.setAttribute("alt", texto);
+    return img;
+}
+
+function Menu() {
+    const lista = document.createElement("ul");
+    const opcoes = [
+        { menu: "Home", rota: "/home" },
+        { menu: "Perfil", rota: "/perfil" },
+        { menu: "Sair", rota: "/login" }
+    ];
+    opcoes.forEach((opcao) => {
+        const link = Link(opcao.menu, opcao.rota);
+        link.addEventListener("click", (event) => {
+            event.preventDefault()
+            navega(event.target.getAttribute("href"));
+        })
+        const item = document.createElement("li");
+        item.append(link);
+        lista.append(item);
+    });
+    const nav = document.createElement("nav");
+    nav.append(lista);
+    return nav;
+}
+
 function FormLogin() {
     const button = Botao("Entrar");
+    const email = Input("email", "email", "E-mail");
+    const senha = Input("senha", "password", "Senha");
 
     const form = document.createElement("form");
     form.setAttribute("action", "");
     form.setAttribute("method", "post");
-    form.append(button);
+    form.append(email, senha, button);
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        // chamada ao backend
+        navega("/home");
+    });
     return form;
+}
+
+function FormPerfil() {
+    const nome = Input("nome", "text", "Nome")
+    const email = Input("email", "email", "E-mail")
+    const senha = Input("senha", "password", "Senha");
+    const salvar = Botao("Salvar")
+    const form = document.createElement("form")
+    form.append(nome, email, senha, salvar)
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        navegar("/perfil")
+    })
+    return form
+}
+
+/* utilitarios */
+
+function navega(rota) {
+    root.innerHTML = "";
+    if (rota === "/login") {
+        Login();
+    } else if (rota === "/home") {
+        Home();
+    } else if (rota === "/perfil") {
+        Perfil();
+    } else {
+        root.innerHTML = "<p>Deu ruim!</p>";
+    }
 }
 
 /* paginas da aplicacao */
 
 function Perfil() {
-    root.append(Cabecalho());
-    root.append(Principal());
+    const cabecalho = Cabecalho()
+    const main = Principal()
+    const menu = Menu()
+    const form = FormPerfil()
+    main.append(menu, form)
+    root.append(cabecalho, main);
 }
 
 function Home() {
-    root.append(Cabecalho());
-    root.append(Principal());
+    const cabecalho = Cabecalho();
+    const main = Principal();
+    const menu = Menu();
+    main.append(menu);
+    root.append(cabecalho, main);
 }
 
 function Login() {
+    const logo = Imagem("https://www.svgrepo.com/show/489120/school.svg", "Logo da Aplicação");
+    const titulo = Titulo("Aluno Online");
+    const link = Link("Esqueceu a Senha?", "/esqueceu-senha");
+    const form = FormLogin();
     const main = Principal();
-    main.append(FormLogin());
-
-    root.append(main);
-    root.append(Rodape());
+    main.setAttribute("class", "login-container");
+    main.append(logo, titulo, form, link);
+    const rodape = Rodape("Copyright (C) 2024");
+    root.append(main, rodape);
 }
 
 Login();
